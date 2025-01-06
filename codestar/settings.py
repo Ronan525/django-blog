6f3e618851c954cd6a4f4b73da12b52b2f39bb06
Ronan Greenhalgh <RonanGreenhalgh@outlook.com>
2025-01-06 16:09:05 +0000

    test files

--- codestar/settings.py
+++ codestar/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile('env.py'):
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-z(bhy9t(c0$+bdlha$!n9-p$oa^d_ea$x+^da#sdpl9zyjwwq^
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-ronan525-djangoblog-o5c0gq5l49u.ws.codeinstitute-ide.net',
@@ -113,6 +114,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
